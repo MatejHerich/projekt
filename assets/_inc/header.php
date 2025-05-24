0c5fafc3bb6e43f49349901cb803ed1c6e6cf0d4
Matej Herich <herich.matej2005@gmail.com>
2025-05-24 11:12:25 +0200

    Vyladenie menších chýb

--- assets/_inc/header.php
+++ assets/_inc/header.php
@@ -41,7 +41,7 @@
         <div class="col-lg-4 col-md-4">
           <ul class="social-links">
             <li><a href="https://www.facebook.com/"><i class="fab fa-facebook"></i></a></li>
-            <li><a href="https://x.com/minthu" target="_blank"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://x.com/minthu"><i class="fab fa-twitter"></i></a></li>
             <li><a href="https://www.linkedin.com/"><i class="fab fa-linkedin"></i></a></li>
             <li><a href="https://www.instagram.com/"><i class="fab fa-instagram"></i></a></li>
           </ul>
